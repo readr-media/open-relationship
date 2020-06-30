a4f6ee64711d10823456db4111eb96cdae9128eb
Hsin-chan Chien <hcchien@gmail.com>
2020-06-30 14:53:17 +0800

    fixed the permission table bugs

--- lists/Permission.js
+++ lists/Permission.js
@@ -1,5 +1,5 @@
 const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.role == 'admin');
 const everyone = ({ authentication: { item: user } }) => Boolean(user);
-const access = { userIsAdmin, everyone, userIsSales, userIsExecutor, userIsPlanner };
+const access = { userIsAdmin, everyone };
 
 module.exports = { "access": access }

4df9a1a4b5671f9b364d9626a6b97690304b159c
Leon Peng <g70245@gmail.com>
2020-09-15 22:56:53 +0800

    add a sample of config

---
+++ configs/config.sample.js
@@ -0,0 +1,14 @@
+/* This configuration follows YAML naming style. */
+
+module.exports = {
+    app: {
+        applicationName: "Open Relationship",
+        dropDatabase: true,
+    },
+    database: {
+        host: "host",
+        db: "db",
+        acc: "acc",
+        pass: "pass",
+    },
+}

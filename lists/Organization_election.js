e4a88908073ba28e1aa7f40e53b1b5a431cbc6c4
“liyibass” <liyibass@mirrormedia.mg>
2020-09-29 15:00:22 +0800

    in keystone, add person_election, organization_election and person_publication.

---
+++ lists/Organization_election.js
@@ -0,0 +1,23 @@
+const { Text, Relationship, Integer } = require("@keystonejs/fields");
+const { atTracking, byTracking } = require("@keystonejs/list-plugins");
+
+module.exports = {
+  fields: {
+    organization_id: {
+      label: "組織名稱",
+      type: Relationship,
+      many: false,
+      ref: "Person",
+    },
+    election_year_year: { label: "選舉年", type: Integer },
+    election_year_month: { label: "選舉月", type: Integer },
+    election_year_day: { label: "選舉日", type: Integer },
+    // election_name: { label: "選舉名稱", type: Text },
+
+    number: { label: "號次", type: Text },
+    votes_obtained_number: { label: "得票數", type: Text },
+    seats: { label: "分配席次", type: Text },
+    source: { label: "資料來源", type: Text },
+  },
+  plugins: [atTracking(), byTracking()],
+};
--- lists/Person_publication.js
+++ lists/Person_publication.js
@@ -1,18 +1,28 @@
-const { Text, Relationship, CalendarDay } = require('@keystonejs/fields');
-const { atTracking, byTracking } = require('@keystonejs/list-plugins');
+const { Text, Relationship, Integer } = require("@keystonejs/fields");
+const { atTracking, byTracking } = require("@keystonejs/list-plugins");
 
 module.exports = {
   fields: {
-    person_id: { label: "姓名", type: Relationship, many: false, ref: 'Person' },
-    organization_id: { label: "組織名稱", type: Relationship, many: false, ref: 'Organization' },
+    person_id: {
+      label: "姓名",
+      type: Relationship,
+      many: false,
+      ref: "Person",
+    },
+    organization_id: {
+      label: "組織名稱",
+      type: Relationship,
+      many: false,
+      ref: "Organization",
+    },
     publication: { label: "著作名稱", type: Text },
-    published_date: { label: "發表時間", type: CalendarDay },
+    // published_date: { label: "發表時間", type: CalendarDay },
+    published_date_year: { label: "發表時間年", type: Integer },
+    published_date_month: { label: "發表時間月", type: Integer },
+    published_date_day: { label: "發表時間日", type: Integer },
     category: { label: "類型", type: Text },
     attachment: { label: "附件", type: Text },
     source: { label: "資料來源", type: Text },
   },
-  plugins: [
-    atTracking(),
-    byTracking(),
-  ],
+  plugins: [atTracking(), byTracking()],
 };

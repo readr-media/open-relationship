ac2bfdbf90ec1e12a6dc7058a458235c8a703231
HY Tan <hsuehyungtan@gmail.com>
2020-10-06 16:14:32 +0800

    refactor: refactor Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,51 +1,33 @@
 <template>
-  <div id="Hero">
-    <div id="Hero-background">
-      <picture>
-        <source
-          media="(min-width:480px)"
-          srcset="
-            ../images/bg-hero-md.png 1x,
-            ../images/bg-hero-md.png 2x,
-            ../images/bg-hero-md.png 3x
-          "
-        />
-        <!-- <source
-          media="(min-width:150px)"
-          srcset="
-            ../images/bg-hero-sm.png 1x,
-            ../images/bg-hero-sm.png 2x,
-            ../images/bg-hero-sm.png 3x
-          "
-        /> -->
-        <img srcset="../images/bg-hero-sm.png" alt="" />
-      </picture>
-    </div>
+  <div class="hero">
+    <a
+      class="hero__logo"
+      href="https://www.readr.tw/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img :src="READrLogo" alt="READr" />
+    </a>
 
-    <img id="Hero-logo" :src="READrLogo" alt="" />
-    <img id="Hero-share" :src="share" alt="" />
+    <!-- <img id="Hero-share" :src="share" alt="" /> -->
 
-    <div id="Hero-rectangle-small">
-      <div id="Hero-rectangle-small-title">
-        臺灣
-        <div>
-          <span class="text-style-1">政商人物</span
-          ><span class="text-style-2">關係</span>
-        </div>
-        資料庫計畫
-      </div>
-    </div>
+    <picture class="hero__background">
+      <source media="(min-width:768px)" srcset="../images/bg-hero-md.png" />
+      <source
+        media="(max-width:767px) and (orientation: landscape)"
+        srcset="../images/bg-hero-md.png"
+      />
+      <img src="../images/bg-hero-sm.png" />
+    </picture>
 
-    <div id="Hero-rectangle-large">
-      <div id="Hero-rectangle-large-title">
-        臺灣
-        <span class="text-style-1">政商人物</span
-        ><span class="text-style-2">關係</span>
-        資料庫計畫
-      </div>
-      <div id="Hero-rectangle-large-content">
+    <div class="hero__content">
+      <h1 class="hero__heading">
+        臺灣<br /><span class="color--orange">政商人物</span
+        ><span class="color--blue">關係</span><br />資料庫計畫
+      </h1>
+      <p class="hero__description">
         「臺灣政商人物關係資料庫計畫」希望用「開放資料」的格式將散佈在網路上關於公眾人物的公開資料串連起來，包括他們的學歷、經歷、資產、企業關係、選舉經驗、政治獻金、學術論文、報告、親戚關係、甚至每一句公開發言。
-      </div>
+      </p>
     </div>
   </div>
 </template>
@@ -66,188 +48,95 @@ export default {
 
 <style lang="scss" scoped>
 @import '../styles/responsiveMixin.scss';
-#Hero {
-  width: 100%;
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+.hero {
   position: relative;
-
-  #Hero-logo {
-    width: 45px;
-    height: 41px;
-    position: absolute;
+  &__logo {
+    position: fixed;
     top: 10px;
     left: 10px;
-  }
-
-  #Hero-share {
-    width: 35px;
-    height: 35px;
-    position: absolute;
-    top: 10px;
-    left: 65px;
-  }
-
-  #Hero-background {
-    z-index: 1;
-
-    width: 100%;
-    height: 100%;
-    img {
-      width: 100%;
-    }
-  }
-  .text-style-1 {
-    color: #ed8c4a;
-  }
-  .text-style-2 {
-    color: #63c1e2;
-  }
-
-  #Hero-rectangle-small-title {
-    z-index: 0;
-    width: 215px;
-    height: 170px;
-    background-color: #102c34;
-
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding: 20px 0 24px;
-
-    font-size: 30px;
-    font-weight: 600;
-
-    text-align: center;
-    color: #ffffff;
-  }
-  // -----------------------------------------------
-
-  #Hero-rectangle-large {
-    display: none;
-    min-width: 58.333%;
-    min-height: 71.111%;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: #102c34;
-
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-
-    &-title {
-      height: 56px;
-      font-family: PingFangTC;
-      font-size: 2rem;
-      font-weight: 600;
-      font-stretch: normal;
-      font-style: normal;
-      line-height: normal;
-      letter-spacing: normal;
-      text-align: center;
-      color: #ffffff;
-
-      margin-bottom: 5px;
-    }
-
-    &-content {
-      width: 495px;
-      // height: 256px;
-      padding: 10px;
-
-      font-family: PingFangTC;
-      font-size: 0.8rem;
-      font-weight: 500;
-      font-stretch: normal;
-      font-style: normal;
-      line-height: 2;
-      letter-spacing: normal;
-      text-align: center;
-      color: #ffffff;
-    }
-  }
-  @include atSmall {
-    position: relative;
-    #Hero-rectangle-large {
-      display: none;
-    }
-  }
-
-  @include atMedium {
-    #Hero-logo {
-      width: 50px;
-      height: 50px;
-
+    width: 45px;
+    height: 41px;
+    z-index: 999;
+    @include atMedium {
       top: 20px;
       left: 20px;
+      width: 50px;
+      height: 50px;
     }
-
-    #Hero-share {
-      width: 45px;
-      height: 45px;
-      position: absolute;
-      top: 20px;
-      left: 90px;
+  }
+  &__background {
+    position: relative;
+    z-index: 1;
+    display: block;
+    img {
+      display: block;
+      width: 100%;
+      height: 100%;
+      max-height: 100vh;
+      object-fit: cover;
+      object-position: center center;
     }
-    #Hero-rectangle-large {
+  }
+  &__content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 215px;
+    padding: 20px 0;
+    color: #fff;
+    text-align: center;
+    background-color: #102c34;
+    @include atMedium {
       display: flex;
-      min-width: auto;
-      min-height: auto;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
       width: 560px;
-      height: 384px;
-
-      &-title {
-        margin-bottom: 5px;
-        font-size: 2rem;
-      }
-
-      &-content {
-        width: 495px;
-        padding: 10px;
-        font-size: 0.8rem;
-      }
+      min-height: 50%;
+      padding: 40px;
+    }
+    @include atLarge {
+      width: 70%;
+      max-width: 840px;
+      padding: 60px 0;
     }
   }
-
-  @include atLarge {
-    #Hero-rectangle-large {
-      width: 700px;
-      height: 480px;
-
-      &-title {
-        margin-bottom: 25px;
-        font-size: 2.5rem;
+  &__heading {
+    font-size: 30px;
+    font-weight: 600;
+    @include atMedium {
+      font-size: 40px;
+      br {
+        display: none;
       }
-
-      &-content {
-        width: 495px;
-
-        font-size: 1rem;
-      }
+    }
+    @include atLarge {
+      font-size: 48px;
     }
   }
-
-  @include atUltraLarge {
-    #Hero-rectangle-large {
-      width: 840px;
-      height: 576px;
-
-      &-title {
-        margin-bottom: 25px;
-        font-size: 3rem;
-      }
-
-      &-content {
-        width: 594px;
-        font-size: 1.2rem;
-      }
+  &__description {
+    display: none;
+    @include atMedium {
+      display: block;
+      width: 495px;
+      margin: 30px auto 0;
+      font-weight: 500;
+      line-height: 2;
     }
+    @include atLarge {
+      font-size: 20px;
+    }
+  }
+}
+
+.color {
+  &--orange {
+    color: #ed8c4a;
+  }
+  &--blue {
+    color: #63c1e2;
   }
 }
 </style>

288bfd97c077e735f4251c9eaf7fc012c20c1e7b
HY Tan <hsuehyungtan@gmail.com>
2020-10-12 16:11:02 +0800

    refactor: refactor Contact component

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,12 +1,15 @@
 <template>
-  <div id="Contact">
-    <div id="Contact-title" class="section-title">
-      留下 email，<br class="changeLine-small" />
-      通知你最新進度！
+  <div class="contact">
+    <div class="contact__title">
+      留下 email，<br class="contact__break-line" />通知你最新進度！
     </div>
-    <form id="Contact-form">
-      <input id="Contact-form-input" type="text" />
-      <Button title="送出" @click="handleClick" />
+    <form class="contact__form">
+      <input class="contact__input" type="email" />
+      <Button
+        class="contact__submit"
+        title="送出"
+        @click.prevent="handleClick"
+      />
     </form>
   </div>
 </template>
@@ -32,46 +35,50 @@ export default {
 <style lang="scss" scoped>
 @import '../styles/responsiveMixin.scss';
 
-#Contact {
-  width: 100%;
+.contact {
   padding: 42px 20px;
   background-color: #ebebeb;
-  overflow: auto;
-
-  #Contact-title {
-    height: auto;
-    margin: 0 auto 20px;
+  > * {
+    max-width: 610px;
+    margin-left: auto;
+    margin-right: auto;
   }
-  #Contact-form {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    #Contact-form-input {
-      height: 50px;
-      font-size: 1rem;
-      box-shadow: inset 0 1px 3px 0 rgba(0, 0, 0, 0.5);
-      background-color: #ffffff;
-      margin-bottom: 10px;
+  &__title {
+    font-size: 21px;
+    font-weight: 600;
+    letter-spacing: 1px;
+    text-align: center;
+  }
+  &__break-line {
+    @include atMedium {
+      display: none;
     }
   }
-
-  @include atSmall {
-    #Contact-form {
-      flex-direction: row;
-      #Contact-form-input {
-        flex: 7;
-        margin-right: 10px;
-      }
-      .Button {
-        flex: 1;
-      }
+  &__form {
+    margin: 20px auto 0;
+    @include atMedium {
+      display: flex;
     }
   }
-
-  @include atMedium {
-    #Contact-form {
-      margin: auto;
-      width: 610px;
+  &__input {
+    display: block;
+    width: 100%;
+    height: 50px;
+    padding: 0 0 0 10px;
+    font-size: 16px;
+    background-color: #ffffff;
+    box-shadow: inset 0 1px 3px 0 rgba(0, 0, 0, 0.5);
+    @include atMedium {
+      flex: 1;
+      width: auto;
+    }
+  }
+  &__submit {
+    width: 100%;
+    margin-top: 10px;
+    @include atMedium {
+      width: 100px;
+      margin: 0 0 0 10px;
     }
   }
 }

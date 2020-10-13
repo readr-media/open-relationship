4c9cff895222e2fd61e4a5f3ef10b849b0ba2a2c
HY Tan <hsuehyungtan@gmail.com>
2020-10-13 15:29:15 +0800

    feat: add email subscription

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,22 +3,44 @@
     <div class="contact__title">
       留下 email，<br class="contact__break-line" />通知你最新進度！
     </div>
-    <form class="contact__form">
-      <input class="contact__input" type="email" />
-      <Button
-        class="contact__submit"
-        title="送出"
-        @click.prevent="handleClick"
-      />
+    <form class="contact__form" @submit.prevent="handleSubmit">
+      <input v-model="email" class="contact__input" type="email" />
+      <Button class="contact__submit" title="送出" @click="handleClick" />
     </form>
+    <p
+      v-if="isSuccess || isError"
+      :class="{ error: isError }"
+      class="contact__message"
+    >
+      {{
+        isSuccess
+          ? '謝謝你的訂閱，您已加入電子報名單。'
+          : '發生錯誤，請稍後再試'
+      }}
+    </p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import dayjs from 'dayjs'
+
 import Button from '../components/Button'
+
+const API_URL =
+  'https://www.readr.tw/project-api/googlesheet?spreadsheet_id=1-7YTT5Y-VINVAYy8Aem0kGnerQfTEoBASQxI_J1YR8c&range=Email&value_input_option=RAW'
+const SLUG = 'whoareyou.readr.tw'
+
 export default {
   components: {
     Button,
+  },
+  data() {
+    return {
+      email: '',
+      isError: false,
+      isSuccess: false,
+    }
   },
   methods: {
     handleClick() {
@@ -27,6 +49,25 @@ export default {
         eventAction: 'click',
         eventLabel: 'send email',
       })
+    },
+    handleSubmit() {
+      this.addToGoogleSheet()
+    },
+    async addToGoogleSheet() {
+      const params = {
+        majorDimension: 'ROWS',
+        values: [[this.email, dayjs().format('YYYYMMDDHHmm'), SLUG, SLUG]],
+      }
+      try {
+        await axios.post(API_URL, params, {
+          headers: { 'Content-Type': 'application/json' },
+        })
+        this.email = ''
+        this.isSuccess = true
+      } catch (error) {
+        this.isError = true
+        console.error('[ERROR] Submit to sheet.', error)
+      }
     },
   },
 }
@@ -61,7 +102,7 @@ export default {
     }
   }
   &__input {
-    display: block;
+    display: inline-block;
     width: 100%;
     height: 50px;
     padding: 0 0 0 10px;
@@ -74,11 +115,20 @@ export default {
     }
   }
   &__submit {
+    display: inline-block;
     width: 100%;
     margin-top: 10px;
     @include atMedium {
       width: 100px;
       margin: 0 0 0 10px;
+    }
+  }
+  &__message {
+    margin-top: 5px;
+    font-size: 12px;
+    text-align: right;
+    &.error {
+      color: red;
     }
   }
 }

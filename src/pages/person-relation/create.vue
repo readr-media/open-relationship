60a1ad9be1b4ff382c4a597ffef88bad8173dae5
HY Tan <hsuehyungtan@gmail.com>
2020-10-27 18:30:00 +0800

    feat: create new persons when creating person-relation

--- src/pages/person-relation/create.vue
+++ src/pages/person-relation/create.vue
@@ -49,6 +49,7 @@ import formMixin from '../../mixins/formMixin'
 import More from '../../components/More'
 import Footer from '../../components/Footer'
 import OtherForms from '../../components/OtherForms'
+import { createPersons } from '~/apollo/queries/persons.gql'
 
 export default {
   name: 'CreatePersonRelation',
@@ -79,10 +80,53 @@ export default {
       },
     }
   },
+  computed: {
+    needCreateCollaborate() {
+      return (
+        this.collaborate.name ||
+        this.collaborate.email ||
+        this.collaborate.feedback
+      )
+    },
+    needCreatePerson() {
+      return !(
+        this.personRelation.person_id.value.id &&
+        this.personRelation.related_person_id.value.id
+      )
+    },
+  },
   mounted() {
     this.clearForm(this.personRelation)
   },
   methods: {
+    buildCreatePersonsVariables() {
+      const variables = []
+      if (!this.personRelation.person_id.value.id) {
+        variables.push(
+          this.buildPersonVariable(
+            this.personRelation.person_id.value.name,
+            this.personRelation.source.value
+          )
+        )
+      }
+      if (!this.personRelation.related_person_id.value.id) {
+        variables.push(
+          this.buildPersonVariable(
+            this.personRelation.related_person_id.value.name,
+            this.personRelation.source.value
+          )
+        )
+      }
+      return { data: variables }
+    },
+    buildPersonVariable(name, source) {
+      return {
+        data: {
+          name,
+          source,
+        },
+      }
+    },
     async uploadHandler() {
       // if there's any form format error,scroll to it.
       if (await !this.checkForm(this.personRelation)) {
@@ -90,25 +134,42 @@ export default {
         return
       }
       this.uploadForm()
-      this.clearForm(this.personRelation)
-      this.$router.push('/thanks')
     },
 
     async uploadForm() {
+      if (this.needCreatePerson) {
+        const resultCreatePersons = await this.$apollo.mutate({
+          mutation: createPersons,
+          variables: this.buildCreatePersonsVariables(),
+        })
+        resultCreatePersons.data.createPersons.forEach((person) => {
+          if (this.personRelation.person_id.value.name === person.name) {
+            this.personRelation.person_id.value.id = person.id
+          } else if (
+            this.personRelation.related_person_id.value.name === person.name
+          ) {
+            this.personRelation.related_person_id.value.id = person.id
+          }
+        })
+      }
       // Upload person form
-      this.$apollo.mutate({
+      await this.$apollo.mutate({
         mutation: ADD_PERSON_RELATION,
-        variables: await moveFormToGqlVariable(this.personRelation),
+        variables: moveFormToGqlVariable(this.personRelation),
       })
       // Update collaborate form
-      this.$apollo.mutate({
-        mutation: ADD_COLLABORATE,
-        variables: {
-          name: this.collaborate.name,
-          email: this.collaborate.email,
-          feedback: this.collaborate.feedback,
-        },
-      })
+      if (this.needCreateCollaborate) {
+        await this.$apollo.mutate({
+          mutation: ADD_COLLABORATE,
+          variables: {
+            name: this.collaborate.name,
+            email: this.collaborate.email,
+            feedback: this.collaborate.feedback,
+          },
+        })
+      }
+      this.clearForm(this.personRelation)
+      this.$router.push('/thanks')
     },
   },
 }

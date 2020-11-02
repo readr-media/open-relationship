88c2ff8d729436847a8b14c121a5227cd6190ee4
HY Tan <hsuehyungtan@gmail.com>
2020-11-02 16:32:01 +0800

    feat: add form link for same name and adjust text

--- src/components/ListSameName.vue
+++ src/components/ListSameName.vue
@@ -1,36 +1,52 @@
 <template>
-  <div v-if="hasItems" class="list">
-    <p>
-      <strong
-        >你要{{ formTypeDisplay }}的是下列{{ formNameDisplay }}嗎？</strong
-      >
-    </p>
-    <div class="list__items">
-      <div v-for="item in items" :key="`${item.name}${item.id}`">
-        <a
-          :href="`/${formName}/verify/${item.id}`"
+  <div v-if="hasItems || showLinkForSameName" class="list">
+    <template v-if="hasItems">
+      <p>
+        <strong
+          >你要{{ formTypeDisplay }}的是下列{{ formNameDisplay }}嗎？</strong
+        >
+      </p>
+      <div class="list__items">
+        <div v-for="item in items" :key="`${item.name}${item.id}`">
+          <a
+            :href="`/${formName}/verify/${item.id}`"
+            :class="`g-color-${formType}`"
+            target="_blank"
+            rel="noopener noreferrer"
+            v-text="item.name"
+          />
+          <!-- 由於不同表單提供的欄位資訊不同，請先組好要顯示的資訊 -->
+          <span v-if="item.info">（{{ item.info }}）</span>
+        </div>
+      </div>
+    </template>
+    <p v-if="showLinkForSameName">
+      <span v-if="hasItems && formType === 'create'"
+        >如果以上不是你要{{ formTypeDisplay }}的{{ formNameDisplay }}，</span
+      ><span v-else-if="hasItems && formType === 'verify'">
+        如果你想要{{ formTypeDisplay }}的{{ formNameDisplay }}與上述{{
+          formNameDisplay
+        }}名稱相同，</span
+      ><span
+        >請填寫<a
           :class="`g-color-${formType}`"
+          :href="linkForAddSameName"
           target="_blank"
           rel="noopener noreferrer"
-          v-text="item.name"
-        />
-        <!-- 由於不同表單提供的欄位資訊不同，請先組好要顯示的資訊 -->
-        <span v-if="item.info">（{{ item.info }}）</span>
-      </div>
-    </div>
-    <p>
-      如果以上皆不是你要{{ formTypeDisplay }}的{{ formNameDisplay }}，請填寫<a
-        :class="`g-color-${formType}`"
-        target="_blank"
-        rel="noopener noreferrer"
-        >表單</a
-      >，我們將為你新增同名的{{ formNameDisplay }}資料
+          >表單</a
+        >，我們將為你新增{{ formNameDisplay }}資料
+      </span>
     </p>
   </div>
 </template>
 
 <script>
 import { ROUTE_MAPPING } from '~/constants'
+
+const LINK_FOR_ADD_SAME_NAME = {
+  person: 'https://forms.gle/cuzHnCb8pE1S31FV7',
+  organization: 'https://forms.gle/ExtoR4116oSndUot6',
+}
 
 export default {
   name: 'ListSameName',
@@ -60,6 +76,15 @@ export default {
     },
     hasItems() {
       return this.items.length > 0
+    },
+    linkForAddSameName() {
+      return LINK_FOR_ADD_SAME_NAME[this.formName] || ''
+    },
+    showLinkForSameName() {
+      return (
+        this.linkForAddSameName &&
+        !(this.formType === 'create' && !this.hasItems)
+      )
     },
   },
 }
